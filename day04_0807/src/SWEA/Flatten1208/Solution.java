c22d46f933373f6218c1d93f23bd64fe48348785
seoliee <dev.seoliee49@gmail.com>
2025-08-08 18:41:19 +0900

    풀었는데 뭔지 모르겠다

--- day04_0807/src/SWEA/Flatten1208/Solution.java
+++ day04_0807/src/SWEA/Flatten1208/Solution.java
@@ -8,8 +8,9 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		File file = new File("./src/SWEA/Flatten1208/input.txt");
-		Scanner sc = new Scanner(file);
+//		File file = new File("./src/SWEA/Flatten1208/input.txt");
+//		Scanner sc = new Scanner(file);
+		Scanner sc = new Scanner(System.in);
 		
 		// 테스트 케이스 10개이므로 10번 반복
 		for (int i = 0; i < 10; i++) {
@@ -23,23 +24,23 @@ public class Solution {
 				arr[j] = sc.nextInt();
 			}
 			
+			// max - min 초기화
+			int result = 99;
 
 			// 덤프 반복 시작
 			for (int j = 0; j < dumpCnt; j++) {
 				
-				// max - min 초기화
-				int result = 99;
-				
 				// 최대값, 최소값 초기화
 				int maxInt = arr[0];
 				int minInt = arr[0];
-					
+				
 				// 최대값, 최소값의 위치 인덱스
 				int maxIdx = 0;
 				int minIdx = 0;
-					
+				
 				// 최대값, 최소값 찾기 시작
 				for (int k = 0; k < 99; k++) {
+					
 					// 최대값 찾는 조건
 					if (maxInt < arr[k + 1]) {
 						maxInt = arr[k + 1];
@@ -57,13 +58,30 @@ public class Solution {
 				arr[minIdx]++;
 				// 덤프 한번 끝났어
 					
-				// result 구해봐
-				result = arr[maxIdx] - arr[minIdx];
-
-				// result가 0이나 1이면 덤프 끝내
-				if (result == 0 || result == 1) break;	
 				
 			}  // 덤프 끝
+			
+			// 덤프 끝나고 최종 result 계산
+			int maxInt = arr[0];
+			int minInt = arr[0];
+			
+			// 다시 최대값, 최소값 구해
+			for (int k = 0; k < 99; k++) {
+				if(maxInt < arr[k+1]) {
+					maxInt = arr[k+1];
+				}
+				if(minInt > arr[k+1]) {
+					minInt = arr[k+1];
+				}
+			}  // 다 구했어
+			
+			// 그러면 이제 result 구해봐
+			result = maxInt - minInt;
+
+			// result가 0이나 1이면 덤프 끝내
+			if (result == 0 || result == 1) {
+				break;
+			}
 			
 			System.out.printf("#%d %d\n", (i + 1), result);
 		}
