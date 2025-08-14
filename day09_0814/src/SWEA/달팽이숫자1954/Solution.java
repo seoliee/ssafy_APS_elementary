9d5a62f5a94451660748274e23b5479b584261ba
seoliee <dev.seoliee49@gmail.com>
2025-08-14 13:53:49 +0900

    달팽이 딱대

---
+++ day09_0814/src/SWEA/달팽이숫자1954/Solution.java
@@ -0,0 +1,42 @@
+package SWEA.달팽이숫자1954;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("./src/SWEA/달팽이숫자1954/input.txt");
+		Scanner sc = new Scanner(file);
+		
+		// 테케 입력받기
+		int T = sc.nextInt();
+		
+		//테케만큼 반복 시작
+		for(int tc = 1; tc <= T; tc++) {
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}  // 테케만큼 반복 끝
+	}
+
+}
