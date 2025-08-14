630232b05597005381be58102d6a40e3fee0bddc
seoliee <dev.seoliee49@gmail.com>
2025-08-14 15:39:31 +0900

    내일 또 보자 달팽아

--- day09_0814/src/SWEA/달팽이숫자1954/Solution.java
+++ day09_0814/src/SWEA/달팽이숫자1954/Solution.java
@@ -2,6 +2,7 @@ package SWEA.달팽이숫자1954;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -15,27 +16,62 @@ public class Solution {
 		
 		//테케만큼 반복 시작
 		for(int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();  // 배열 크기 입력받기
+			int[][] snail = new int[N][N];  // 배열 선언하기
 			
+			int num = 1;  // 배열에 들어갈 숫자 선언 및 초기화
 			
+			// 배열의 첫 줄 채우기
+			for(int c = 0; c < N; c++) {
+				snail[0][c] = num++;
+			}
 			
+			// 배열의 상하좌우 범위 설정
+			int top = 1;
+			int bottom = N - 1;
+			int left = 0;
+			int right = N - 1;
 			
+			while(left <= right && top <= right) {
+				for(int r = top; r <= bottom; r++) {
+					snail[r][right] = num++;
+				}
+				right--;
+				if(left > right || top > bottom) {
+					break;
+				}
+				
+				for(int c = right; c >= left; c--) {
+					snail[bottom][c] = num++;
+				}
+				bottom--;
+				if(left > right || top > bottom) {
+					break;
+				}
+				
+				for(int r = bottom; r >= top; r--) {
+					snail[r][left] = num++;
+				}
+				left++;
+				if(left > right || top > bottom) {
+					break;
+				}
+				
+				for(int c = left; c <= right; c++) {
+					snail[top][c] = num++;
+				}
+				top++;
+			}
 			
+			System.out.println("#" + tc);
+			for(int r = 0; r < N; r++) {
+				for(int c = 0; c < N; c++) {
+					System.out.print(snail[r][c] + " ");
+				}
+				System.out.println();
+			}
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
 		}  // 테케만큼 반복 끝
 	}
 
---
+++ day09_0814/src/SWEA/의석이의세로로말해요5356/Solution.java
@@ -0,0 +1,10 @@
+package SWEA.의석이의세로로말해요5356;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
